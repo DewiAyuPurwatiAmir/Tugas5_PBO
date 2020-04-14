e77bea0fc7922912dcf615c9b0bfd3ca6df73f14
DewiAyuPurwatiAmir <61864918+DewiAyuPurwatiAmir@users.noreply.github.com>
2020-04-14 07:36:46 -0700

    Add files via upload

---
+++ DemoSatu.java
@@ -0,0 +1,9 @@
+class DemoSatu {
+public static void main (String[] args){
+System.out.println("diawal program");
+int x = 30;
+x = x / 0;
+System.out.println(x);
+ System.out.println("diakhir program");
+}
+}
